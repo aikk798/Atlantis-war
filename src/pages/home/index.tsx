a465a4b00833926612a7b3ba79c04ffee78e213e
leaf <leaf@nextxlabs.com>
2024-02-03 15:46:06 +0800

    docs

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,16 +5,25 @@ import Logo from "@/assets/img/logo.png";
 import Blast from "@/assets/img/blast.png";
 import Hill from "@/assets/img/hill.png";
 import LogoMini from "@/assets/img/logo_mini.png";
+import Cloud1 from "@/assets/img/cloud.png";
+import Cloud2 from "@/assets/img/cloud2.png";
+import cloud3 from "@/assets/img/cloud3.png";
+import Cloud4 from "@/assets/img/cloud.png";
+import classNames from "classnames";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <img src={Cloud1} width={200} className={classNames(styles.Cloud, styles.Cloud1)} />
+      <img src={Cloud2} width={160} className={classNames(styles.Cloud, styles.Cloud2)} />
+      <img src={cloud3} width={200} className={classNames(styles.Cloud, styles.Cloud3)} />
+      <img src={Cloud4} width={140} className={classNames(styles.Cloud, styles.Cloud4)} />
       <header className={styles.header}>
         <div className="flex" style={{ gap: 64 }}>
-          <img src={LogoMini} width={60} />
+          <img src={LogoMini} width={50} />
         </div>
         <div className="flex justify-center">
-          <img src={Logo} width={300} />
+          <img src={Logo} width={240} />
         </div>
         <div className="flex justify-end" style={{ gap: 64 }}>
           <a>Twitter</a>
@@ -25,21 +34,21 @@ export default function Home() {
         <div className={styles.banner}>
           <div className={styles.build}>
             Build on
-            <img src={Blast} width={170} />
+            <img src={Blast} width={140} />
           </div>
           <div className={styles.slogen}>EXPLORE ATLANTIS AND UNCOVER NEW GAMING ADVENTURES</div>
           <div className={styles.des}>
-            In the rich and mysterious land of the Kingdom of Atlantis, there lies both danger and
-            opportunity; warriors, master your skills and seize fortune!
+            In The Rich And Mysterious Land Of The Kingdom Of Atlantis, There Lies Both Danger And
+            Opportunity; Warriors, Master Your Skills And Seize Fortune!
           </div>
           <Button className={styles.button}>Coming Soon</Button>
         </div>
-        <img src={Hill} />
+        <img className={styles.hill} src={Hill} />
       </section>
       <div className={styles.trait}>
-        <div>Free To earn</div>
-        <div>Assets of your own</div>
-        <div>Boost User Yields</div>
+        <div>FREE TO EARN</div>
+        <div>ASSETS OF YOUR OWN</div>
+        <div>BOOST USER YIELDS</div>
       </div>
     </div>
   );
